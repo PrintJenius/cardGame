9b0857dbaebd0bf13cee14a432f997a2fe7ba443
printJenius <uopl178@naver.com>
2024-05-21 17:24:46 +0900

    [refactor] 기능에 맞는 식별자 명으로 리팩토링 및 잉여 기능 제거

--- js/stage.js
+++ js/stage.js
@@ -2,37 +2,29 @@ let cardArea = document.getElementById("cardArea");
 let timerArea = document.getElementById("timerArea");
 let startButton = document.getElementById("start");
 let hintButton = document.getElementById("hint");
-let frontCards = [];
-let backCards = [];
-let frontCardsRec = [];
-let backCardsRec = [];
-let playTime = 0;
-let playOnOff = false;
-let timerInterval;
-let remainTimerInterval;
 let chanceObj = document.getElementById("chance");
 let timeObj = document.getElementById("time");
-let remainTimerOnOff = false;
 
-function output() {
-    for (let i = 0; i < backCards.length; i++) {
-        cardArea.appendChild(backCards[i]);
-    }
-}
+let hiddenDeck = [];
+let visibleDeck = [];
+let hiddenDeckRec = [];
+let visibleDeckRec = [];
 
-function update() {
-    cardArea.innerHTML = "";
-    output();
-}
+let playOnOff = false;
+let timerInterval;
+let timeInterval;
+let timerOnOff = false;
 
-function frontCardsCreate() {
+let allCardNum = 26;
+
+function hiddenDeckCreate() {
     for (let i = 0; i < 13; i++) {
         let frontCard = document.createElement("img");
         frontCard.className = "frontCard";
         frontCard.alt = i;
         frontCard.id = i;
         frontCard.src = "card_img\\card_img\\" + i + ".png";
-        frontCards.push(frontCard);
+        hiddenDeck.push(frontCard);
     }
 
     for (let i = 0; i < 13; i++) {
@@ -41,11 +33,11 @@ function frontCardsCreate() {
         frontCard.alt = (i + 13);
         frontCard.id = (i * -1);
         frontCard.src = "card_img\\card_img\\" + (i + 13) + ".png";
-        frontCards.push(frontCard);
+        hiddenDeck.push(frontCard);
     }
 }
 
-function backCardsCreate() {
+function visibleDeckCreate() {
     for (let i = 0; i < 26; i++) {
         let backCard = document.createElement("img");
         backCard.className = "backCard";
@@ -54,68 +46,80 @@ function backCardsCreate() {
             cardClick(this);
         };
         backCard.src = "card_img\\card_img\\back.png";
-        backCards.push(backCard);
+        visibleDeck.push(backCard);
     }
 }
 
-function allBack() {
-    for (let i = 0; i < backCards.length; i++) {
-        if (frontCards[i].className === "backCard")
-            flip(frontCards[i]);
+function displayCardsUpdate() {
+    cardArea.innerHTML = "";
+    for (let i = 0; i < visibleDeck.length; i++) {
+        cardArea.appendChild(visibleDeck[i]);
     }
-    createRec();
+}
+
+
+
+function allBack() {
+    for (let i = 0; i < visibleDeck.length; i++) {
+        if (hiddenDeck[i].className === "backCard")
+            flip(hiddenDeck[i]);
+    }
+    recordCardState();
 }
 
 function allFront() {
-    for (let i = 0; i < backCards.length; i++) {
-        if (backCards[i].className === "backCard")
-            flip(backCards[i]);
+    for (let i = 0; i < visibleDeck.length; i++) {
+        if (visibleDeck[i].className === "backCard")
+            flip(visibleDeck[i]);
     }
-    createRec();
+    recordCardState();
 }
 
 function shuffle() {
-    if (frontCardNum() % 2 === 0) {
-        for (let i = 0; i < frontCards.length; i++) {
-            if (frontCards[i].className === "frontCard") {
+    if (visiblefrontCardNum() % 2 === 0) {
+        for (let i = 0; i < hiddenDeck.length; i++) {
+            if (hiddenDeck[i].className === "frontCard") {
                 let tmp;
-                let random = Math.floor(Math.random() * (frontCards.length));
-                if (frontCards[random].className === "frontCard") {
-                    tmp = frontCards[i];
-                    frontCards[i] = frontCards[random];
-                    frontCards[random] = tmp;
+                let random = Math.floor(Math.random() * (hiddenDeck.length));
+                if (hiddenDeck[random].className === "frontCard") {
+                    tmp = hiddenDeck[i];
+                    hiddenDeck[i] = hiddenDeck[random];
+                    hiddenDeck[random] = tmp;
                 }
             }
         }
-        update();
-        createRec();
+        displayCardsUpdate();
+        recordCardState();
     }
 }
 
-function hint() {
-    if (frontCardNum() % 2 === 0 && remainingChance > 0) {
+function useHint() {
+    if(visiblefrontCardNum() % 2 === 0 && remainingChance > 0) {
         hintOnclickRemove();
         allFront();
         setTimeout(function () {
-            frontCards = frontCardsRec[1].slice();
-            backCards = backCardsRec[1].slice();
-            createRec();
-            update();
+            hiddenDeck = hiddenDeckRec[1].slice();
+            visibleDeck = visibleDeckRec[1].slice();
+            recordCardState();
+            displayCardsUpdate();
         }, 500)
         setTimeout(function () {
             hintOnclickCreate();
         }, 500)
+    remainingChance--;
+    chanceObj.innerHTML = "";
+    chanceObj.innerHTML = remainingChance;
     }
 }
 
-function startTimer() {
-    remainTimerOnOff = !remainTimerOnOff
-    if (remainTimerOnOff) {
-        remainTimerInterval = setInterval(function () {
+function startCountdown() {
+    timerOnOff = !timerOnOff
+    if (timerOnOff) {
+        timeInterval = setInterval(function () {
             if(remainingTime === 0){
-                clearInterval(remainTimerInterval);
+                clearInterval(timeInterval);
                 window.alert("실패T_T");
-                onclickRemove(); // 더 이상 카드를 뒤집을 수 없게
+                disableCardClick(); // 더 이상 카드를 뒤집을 수 없게
             } else {
                 remainingTime--;
                 timeObj.innerHTML = "";
@@ -125,89 +129,70 @@ function startTimer() {
     }
 }
 
-function timerOnOff() {
-    playOnOff = !playOnOff;
-    if (playOnOff) {
-        timerInterval = setInterval(function () {
-            playTime++;
-            timerArea.innerHTML = "";
-            timerArea.innerHTML = playTime ;
-        }, 1000)
-        startButton.innerHTML = "";
-        startButton.innerHTML = "정지";
-    } else {
-        clearInterval(timerInterval);
-        startButton.innerHTML = "";
-        startButton.innerHTML = "시작";
+function recordCardState() {
+    if (!(hiddenDeckRec[1] === undefined))
+        hiddenDeckRec[2] = hiddenDeckRec[1].slice();
+    if (!(hiddenDeckRec[0] === undefined))
+        hiddenDeckRec[1] = hiddenDeckRec[0].slice();
+    hiddenDeckRec[0] = hiddenDeck.slice();
+    if (!(visibleDeckRec[1] === undefined))
+        visibleDeckRec[2] = visibleDeckRec[1].slice();
+    if (!(visibleDeckRec[0] === undefined)) {
+        visibleDeckRec[1] = visibleDeckRec[0].slice();
     }
-}
-
-function useHint() {
-    if(frontCardNum() % 2 === 0 && remainingChance > 0) {
-    remainingChance--;
-    chanceObj.innerHTML = "";
-    chanceObj.innerHTML = remainingChance;
-    }
-}
-
-function createRec() {
-    if (!(frontCardsRec[1] === undefined))
-        frontCardsRec[2] = frontCardsRec[1].slice();
-    if (!(frontCardsRec[0] === undefined))
-        frontCardsRec[1] = frontCardsRec[0].slice();
-    frontCardsRec[0] = frontCards.slice();
-    if (!(backCardsRec[1] === undefined))
-        backCardsRec[2] = backCardsRec[1].slice();
-    if (!(backCardsRec[0] === undefined)) {
-        backCardsRec[1] = backCardsRec[0].slice();
-    }
-    backCardsRec[0] = backCards.slice();
+    visibleDeckRec[0] = visibleDeck.slice();
 }
 
 function flip(card) {
     idx = parseInt(card.alt, 10);
-    let tmp = frontCards[idx];
-    frontCards[idx] = backCards[idx];
-    backCards[idx] = tmp;
-    update();
+
+    let tmp = hiddenDeck[idx];
+    hiddenDeck[idx] = visibleDeck[idx];
+    visibleDeck[idx] = tmp;
+
+    displayCardsUpdate();
 }
 
-function frontCardNum() {
+function visiblefrontCardNum() {
     let cnt = 0;
-    for (let i = 0; i < backCards.length; i++) {
-        if (backCards[i].className === "frontCard")
+    for (let i = 0; i < visibleDeck.length; i++) {
+        if (visibleDeck[i].className === "frontCard")
             cnt++;
     }
     return cnt;
 }
 
-function rightAnswerCheck() {
+function isMatch() {
     let point = 0;
-    for (i = 0; i < backCards.length; i++) {
-        if (backCards[i].className === "frontCard") {
-            point += parseInt(backCards[i].id, 10);
+    let match = false
+    for (i = 0; i < visibleDeck.length; i++) {
+        if (visibleDeck[i].className === "frontCard") {
+            point += parseInt(visibleDeck[i].id, 10);
         }
     }
-    return point;
+    if(point === 0) {
+        match = true;
+    }
+    return match;
 }
 
-function onclickRemove() {
-    for (let i = 0; i < backCards.length; i++) {
-        if (backCards[i].className === "backCard")
-            backCards[i].onclick = null;
-        if (frontCards[i].className === "backCard")
-            frontCards[i].onclick = null;
+function disableCardClick() {
+    for (let i = 0; i < visibleDeck.length; i++) {
+        if (visibleDeck[i].className === "backCard")
+            visibleDeck[i].onclick = null;
+        if (hiddenDeck[i].className === "backCard")
+            hiddenDeck[i].onclick = null;
     }
 }
 
-function onclickCreate() {
-    for (let i = 0; i < backCards.length; i++) {
-        if (backCards[i].className === "backCard")
-            backCards[i].onclick = function () {
+function enableCardClick() {
+    for (let i = 0; i < visibleDeck.length; i++) {
+        if (visibleDeck[i].className === "backCard")
+            visibleDeck[i].onclick = function () {
                 cardClick(this);
             }
-        if (frontCards[i].className === "backCard")
-            frontCards[i].onclick = function () {
+        if (hiddenDeck[i].className === "backCard")
+            hiddenDeck[i].onclick = function () {
                 cardClick(this);
             }
     }
@@ -219,7 +204,6 @@ function hintOnclickRemove() {
 
 function hintOnclickCreate() {
     hintButton.onclick = function () {
-        hint();
         useHint();
     }
 }
@@ -228,33 +212,37 @@ function cardClick(card) { // 카드를 클릭했을 때
 
     flip(card); // 카드 뒤집기
 
+    recordCardState(); // 이전 카드배열 내용 기록(되돌아 가기 위함)
 
-    createRec(); // 이전 카드배열 내용 기록(되돌아 가기 위함)
+    if (visiblefrontCardNum() % 2 === 0) { // 카드 중 앞면 카드가 짝수개 일 때 마다
 
-    if (frontCardNum() % 2 === 0) { // 카드 중 앞면 카드가 짝수개 일 때 마다
-
-        if (!(rightAnswerCheck() === 0)) {
-            update();
-            onclickRemove();
+        if (!isMatch()) {
+            disableCardClick();
             setTimeout(function () {
-                frontCards = frontCardsRec[2].slice();
-                backCards = backCardsRec[2].slice();
-                update();
-                createRec();
+                hiddenDeck = hiddenDeckRec[2].slice();
+                visibleDeck = visibleDeckRec[2].slice();
+                displayCardsUpdate();
+                recordCardState();
             }, 200)
             setTimeout(function () {
-                onclickCreate();
+                enableCardClick();
             }, 200)
-
         }
+
     }
 
-    if (frontCardNum() === 26) { // 클릭해서 뒤집은 카드가 전부 앞면일때
-        update();
-        clearInterval(remainTimerInterval);
+    if (visiblefrontCardNum() === allCardNum) { 
+        clearInterval(timeInterval);
         setTimeout(function () {
             window.alert("축하합니다. 성공하셨습니다.");
         }, 200)
     }
 }
 
+hiddenDeckCreate();
+visibleDeckCreate();
+displayCardsUpdate();
+shuffle();
+shuffle();
+recordCardState();
+startCountdown();
